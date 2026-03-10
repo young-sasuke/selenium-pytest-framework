594e151d15876a20ae41555799a87e5922c5c904
Uttam Anand <https://github.com/young-sasuke>
2026-03-10 11:54:06 +0530

    fix waits for simple computer page

--- selenium_training/pom_project/pom/simple_comp_page.py
+++ selenium_training/pom_project/pom/simple_comp_page.py
@@ -1,31 +1,36 @@
 import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from selenium_training.pom_project.object_repository.simpcomp_page_locators import SimpCompPageLocators
 
-loc =SimpCompPageLocators()
+loc = SimpCompPageLocators()
+
+
 class Simple_Comp:
+
     def __init__(self, driver):
         self.driver = driver
         self.ac = ActionChains(driver)
+        self.wait = WebDriverWait(driver, 20)
 
     def choose_processor(self):
-        from selenium.webdriver.support.ui import WebDriverWait
-        from selenium.webdriver.support import expected_conditions as EC
-    
-        ele = WebDriverWait(self.driver,10).until(
-        EC.element_to_be_clickable(loc.processor)
+        ele = self.wait.until(
+            EC.element_to_be_clickable(loc.processor)
         )
         ele.click()
         time.sleep(2)
 
     def click_to_addtocart(self):
         # self.driver.find_element('xpath', '//input[@id="add-to-cart-button-75"]').click()
-        self.driver.find_element(*loc.addtocart_btn).click()
+        ele = self.wait.until(
+            EC.element_to_be_clickable(loc.addtocart_btn)
+        )
+        ele.click()
         time.sleep(2)
 
     def go_to_home(self):
-
         self.ac.send_keys(Keys.HOME).perform()
         time.sleep(2)
