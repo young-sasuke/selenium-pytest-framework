f452fa56662d32d2a68d15b920365c25fd545fda
Uttam Anand <https://github.com/young-sasuke>
2026-03-10 12:15:08 +0530

    fix cart page waits for CI

--- selenium_training/pom_project/object_repository/cartpage_locators.py
+++ selenium_training/pom_project/object_repository/cartpage_locators.py
@@ -1,8 +1,8 @@
 class CartPageLocators:
 
 
-    country_data = 'xpath', '//select[@class="country-input"]'
-    state_data = 'xpath', '//select[@class="state-input"]'
-    postalcode = 'xpath', '//input[@id="ZipPostalCode"]'
-    terms_service = 'xpath', '//input[@id="termsofservice"]'
-    checkout_btn = 'xpath', '//button[@id="checkout"]'
+    country_data = ("xpath", '//select[@id="CountryId"]')
+    state_data = ("xpath", '//select[@class="state-input"]')
+    postalcode = ("xpath", '//input[@id="ZipPostalCode"]')
+    terms_service = ("xpath", '//input[@id="termsofservice"]')
+    checkout_btn = ("xpath", '//button[@id="checkout"]')
--- selenium_training/pom_project/pom/cartpage.py
+++ selenium_training/pom_project/pom/cartpage.py
@@ -1,40 +1,68 @@
-
 import time
 from selenium.webdriver.support.select import Select
-
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from selenium_training.pom_project.object_repository.cartpage_locators import CartPageLocators
 
 loc = CartPageLocators()
 
+
 class CartPage:
+
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 20)
 
-    def select_country(self,country):
-        # country_select = self.driver.find_element('xpath', '//select[@class="country-input"]')
-        country_select = self.driver.find_element(*loc.country_data)
+    def select_country(self, country):
+
+        country_select = self.wait.until(
+            EC.presence_of_element_located(loc.country_data)
+        )
+
         select_country = Select(country_select)
         select_country.select_by_visible_text(country)
 
+        time.sleep(2)
+
     def select_state(self):
-        # state_select = self.driver.find_element('xpath', '//select[@class="state-input"]')
-        state_select = self.driver.find_element(*loc.state_data)
+
+        state_select = self.wait.until(
+            EC.presence_of_element_located(loc.state_data)
+        )
+
         select_state = Select(state_select)
         select_state.select_by_visible_text("Other (Non US)")
 
-    def enter_post(self,pincode):
-        # self.driver.find_element('xpath', '//input[@id="ZipPostalCode"]').send_keys(pincode)
-        self.driver.find_element(*loc.postalcode).send_keys(pincode)
+        time.sleep(2)
+
+    def enter_post(self, pincode):
+
+        post = self.wait.until(
+            EC.presence_of_element_located(loc.postalcode)
+        )
+
+        post.clear()
+        post.send_keys(pincode)
+
         time.sleep(2)
 
     def click_on_terms(self):
-        # self.driver.find_element('xpath', '//input[@id="termsofservice"]').click()
-        self.driver.find_element(*loc.terms_service).click()
+
+        terms = self.wait.until(
+            EC.element_to_be_clickable(loc.terms_service)
+        )
+
+        terms.click()
+
         time.sleep(2)
 
     def clcik_on_check_out(self):
-        # self.driver.find_element('xpath', '//button[@id="checkout"]').click()
-        self.driver.find_element(*loc.checkout_btn).click()
+
+        checkout = self.wait.until(
+            EC.element_to_be_clickable(loc.checkout_btn)
+        )
+
+        checkout.click()
+
         time.sleep(2)
