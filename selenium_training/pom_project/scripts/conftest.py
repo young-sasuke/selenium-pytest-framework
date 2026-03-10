60b523df89af80d631cebe88f1a8afa3c31234ba
Uttam Anand <https://github.com/young-sasuke>
2026-03-10 14:21:09 +0530

    refactor

--- selenium_training/pom_project/scripts/conftest.py
+++ selenium_training/pom_project/scripts/conftest.py
@@ -1,18 +1,14 @@
 import pytest
 from selenium import webdriver
 
-
 @pytest.fixture()
 def setup():
 
     chrome_options = webdriver.ChromeOptions()
 
-    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--disable-notifications")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(options=chrome_options)
 
