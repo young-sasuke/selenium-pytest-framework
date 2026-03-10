cba8dcefdf511308f98276e20ae00f987ef61898
Uttam Anand <https://github.com/young-sasuke>
2026-03-10 11:23:30 +0530

    fix hover and desktop click for Jenkins

--- selenium_training/pom_project/pom/homepage.py
+++ selenium_training/pom_project/pom/homepage.py
@@ -15,28 +15,23 @@ class HomePage:
         self.wait = WebDriverWait(driver, 10)
 
     def click_login(self):
-        # self.driver.find_element("xpath", '//a[text()="Log in"]').click()
         self.driver.find_element(*loc.login_link).click()
         time.sleep(4)
 
     def check_login(self):
-        # self.wait.until(EC.visibility_of_element_located(('xpath', '//a[@class="ico-logout"]')))
         self.wait.until(EC.visibility_of_element_located(loc.logout_link))
         time.sleep(2)
 
     def hovor_to_computers(self):
-        # ele = self.driver.find_element("xpath", '(//a[contains(text(),"Computers")])[1]')
-        ele = self.driver.find_element(*loc.computers)
+        ele = self.wait.until(EC.visibility_of_element_located(loc.computers))
         self.ac.move_to_element(ele).perform()
         time.sleep(2)
 
     def click_to_desktop(self):
-        # self.driver.find_element("xpath", '(//a[contains(text(),"Desktops")])[1]').click()
-        ele = self.wait.until(EC.element_to_be_clickable(loc.desktops))
+        ele = self.wait.until(EC.visibility_of_element_located(loc.desktops))
         ele.click()
         time.sleep(4)
 
     def click_to_soppingcart(self):
-        # self.driver.find_element('xpath', '//span[text()="Shopping cart"]').click()
         self.driver.find_element(*loc.shopping_cart).click()
         time.sleep(3)
